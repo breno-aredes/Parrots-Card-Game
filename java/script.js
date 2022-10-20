8a7275b7a93518e06633d1d4340d729bcb2f764d
breno-aredes <breno_mg10@hotmail.com>
2022-10-20 16:53:03 -0300

    v 0.3.2 (lista de gifs criada)

--- java/script.js
+++ java/script.js
@@ -1,5 +1,12 @@
+// cria lista com todos elementos 
+let listaGifs = [`/img/bobrossparrot.gif`, `/img/bobrossparrot.gif`, `/img/explodyparrot.gif`,
+    `/img/explodyparrot.gif`, `/img/fiestaparrot.gif`, `/img/fiestaparrot.gif`, `/img/metalparrot.gif`,
+    `/img/metalparrot.gif`, `/img/revertitparrot.gif`, `/img/revertitparrot.gif`, `/img/tripletsparrot.gif`,
+    `/img/tripletsparrot.gif`, `/img/unicornparrot.gif`, `/img/unicornparrot.gif`];
 
-//função para adicionar cartas.
+
+
+//função para adicionar cartas de acondo com o numero digitado.
 function adicionarcartas() {
 
     //Pergunta ao usuraio numero de cartas.
@@ -8,26 +15,34 @@ function adicionarcartas() {
     let resto = perguntaNumero % 2;
     //divide por 2 para definir o numero de duplas. 
     let metade = perguntaNumero / 2;
-
+    //nova lista onde sera distribuidos todos os elementos de acordo com o numero digitado .
+    let novalista = [];
+    //função que fomar a nova lista
+    for (i = 0; perguntaNumero > i; i++) {
+        novalista[i] = listaGifs[i];
+        alert(novalista);
+    }
 
     //se for maior que 4, menor que 14 e par.
     if (perguntaNumero >= 4 && perguntaNumero <= 14 && resto === 0) {
 
+
         //enquanto o contador for menor que a metade adiciona dupla de cartas.
         for (i = 0; metade > i; i++) {
             const adicioanarCartas = document.querySelector("ul");
+
             adicioanarCartas.innerHTML += `<li>
         <div class="dupla">
             <div onclick="virar(this)" class="carta">
                 <img class="parrot" src="./img/back.png" alt="">
-                <img class="gif sumir" src="./img/bobrossparrot.gif" alt="">
+                <img class="gif sumir" src=".${novalista[i]}" alt=""> 
             </div>
             <div onclick="virar(this)" class="carta">
                 <img class="parrot" src="./img/back.png" alt="">
-                <img class="gif sumir" src="./img/bobrossparrot.gif" alt="">
+                <img class="gif sumir" src=".${novalista[metade + i]}" alt="">
             </div>
         </div>
-    </li>`;
+    </li>` // nota: lista[i] para a primeira linha lista[metade+i] para a segunda linha
         }
 
     }
@@ -45,3 +60,11 @@ function virar(elemento) {
     elemento.children[0].classList.toggle("sumir");
     elemento.children[1].classList.toggle("sumir");
 }
+
+
+// Embaralhar a lista
+// distribuir a lista nas duas colunas
+// Comparar a igualdade das imagens ao serem viradas
+// fixar se iguais / desvirar se diferentes
+// adicionar contador x de jogadas
+// exibir mensagem "Você ganhou em x jogadas!"
